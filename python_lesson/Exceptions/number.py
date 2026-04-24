577028df42d7d9abe38d0fa9de25643a9b2a7d64
Samastery237 <samasterygamer57@gmail.com>
2026-04-24 14:25:56 +0800

    some changes

--- python_lesson/Exceptions/number.py
+++ python_lesson/Exceptions/number.py
@@ -1,12 +1,12 @@
 def main():
-    x = get_in()
+    x = get_in("What's x? ")
     print(f"x is {x}")
 
 
-def get_in():
+def get_in(prompt):
     while True:
         try:
-            return int(input("What's x?"))            
+            return int(input("What's x? "))            
         except ValueError:
             pass
 
